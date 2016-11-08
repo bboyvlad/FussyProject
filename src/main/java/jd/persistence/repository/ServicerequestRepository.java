cb7a3a4d2df07bc69cf9953d36ffdab9c240d18a
Eduardo Fernandez <efernandez.ve@gmail.com>
2016-11-08 11:28:03 -0430

    Service Request:
    Muestra los services requests abiertos:
    
    http://localhost:8080/servicerequest/manage/open
    
    Muestra los Services requests cerrados:
    
    http://localhost:8080/servicerequest/manage/close
    
    Muestra todos los Services Requests
    
    http://localhost:8080/servicerequest/manage/all
    
    /*Role Admin*/

--- src/main/java/jd/persistence/repository/ServicerequestRepository.java
+++ src/main/java/jd/persistence/repository/ServicerequestRepository.java
@@ -3,8 +3,12 @@ package jd.persistence.repository;
 import jd.persistence.model.Servicerequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by eduardom on 10/8/16.
  */
 public interface ServicerequestRepository extends JpaRepository<Servicerequest,Long> {
+    List<Servicerequest> findByClosedFalse();
+    List<Servicerequest> findByClosedTrue();
 }
