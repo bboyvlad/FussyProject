25f6947ef4ced694cfe1d314e29df59550b976db
Eduardo Fernandez <efernandez.ve@gmail.com>
2016-11-14 11:53:44 -0430

    Generate Ticket FINAL

--- src/main/java/jd/persistence/repository/DeferedpayRepository.java
+++ src/main/java/jd/persistence/repository/DeferedpayRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Created by eduardom on 11/10/16.
  */
 public interface DeferedpayRepository extends JpaRepository<Deferedpay,Long> {
+    Deferedpay findByServicerequestIs(long id);
 }
